158c3b15af596d34b6095373acf103b166a0b920
knoepdan <daniel.knoepfel@live.de>
2023-01-23 02:32:22 +0100

    assembly journemans piece

--- as_3_programming/Assembly_Tasks/factorial.asm
+++ as_3_programming/Assembly_Tasks/factorial.asm
@@ -1,8 +1,8 @@
 ;; -----------------------------------------------------------------------------
 ;; LINUX
-;;
+;;   (inspired by case.asm and methods-call.asm)
 ;; Assemble, link and run using the following:
-;;   nasm case.asm -f elf64 -o prog.o && gcc -o prog prog.o && ./prog
+;;   nasm factorial.asm -f elf64 -o factorial.o && gcc factorial.o -o factorial && ./factorial 5
 ;; -----------------------------------------------------------------------------
 global      main
 extern      printf
@@ -19,73 +19,27 @@ section     .text
         call        atoi wrt ..plt  ; calls atoi and stores value in rax
 
 
-    ; #### strategie
-    ; In register RAX we have to number that is to be factorized. 
+    ; #### approach (now that we have passed argument as number in RAX)
+    ; In register RAX we have the initial number to be factorized. 
     ; In one register RBX we now keep the result (initialized with the value of RAX)
     ; We do a loop where we decrement the value in RAX each time but multiply RAX with RBX 
-    ; Once RAX is 0 we jump out of the loop (aka continue) as we have our result
-
-    ; rax: counter to be decremented
-    ; rbx: result (temporary result)
-    ; r14 ??
-
-         mov         r14, rax ; keep original value for cmp
+    ; Once RAX is 1 we jump out of the loop (aka continue) as we have our result
          mov         rbx, rax ; init result in rbx and rax (counter)
     loop:            
         cmp         rax, 1;   ;  rax-1
-        jle         resFound
-       ; mov         rbx, rax ; init result in rbx and rax
-     
-
-        mov         r14, rax ; keep counter as "imul" will overwrite register (extremly unelegant)
-
-        dec         rax ; decrement counter
-        imul        rbx, rax  ; result is stored in rbx
-
-
-
-      ;  mov         rcx, rax ; factor to be decremented
-      ;  dec         rax 
-    ;    imul        rbx, rax  ; result should be stored in r14
-        jmp         loop
-
-     ;----- TESTING
-       ; imul        rax, rbx  ; result is stored in rax
-       ; mov         rsi, rax
-
-     ;------- TESTING
-     
-     
-      ;  mov         rcx, rax ; factor to be decremented
-      ;  dec         rax 
-    ;    imul        rbx, rax  ; result should be stored in r14
-    ;    jmp         loop
+        jle         resFound  ; check flags: jump out of the loop if necessary (rax is 1 or below)
+        dec         rax ; decrement counter rax
+        imul        rbx, rax  ; mulitply temporary result with counter (factorize)
+        jmp         loop  ; 
     resFound: 
-        ; cmp         rax, 3   ; DUMMY..
-
-
-
-
-       mov         rsi, rbx    ; move number to rsi so it can be printed
-        jmp         printNumber
-
-
-    jump:
-        cmp         rax, 3
-        ja          dflt
-        lea         rbx, [tab]
-        jmp         [rbx + (rax - 1) * 8]
-    C1:
-        mov         rsi, 2
-        jmp         print
-    C2:
-        mov         rsi, 3
-        jmp         print
-    C3:
-        mov         rsi, 5
-        jmp         print
-    dflt:
-        mov         rsi, 8
+       mov         r14, rbx    ; save result in r14
+       ; print text  (ok not all on one line)
+        lea         rsi, [rel constmsg]
+        mov         rdx, 15
+        call        print
+       ; print number
+       mov         rsi, r14    ; move number to rsi so it can be printed
+       jmp         printNumber
     print: 
         mov     rdi, 0x0    ; file descriptor: stdout
         mov     rax, 0x1    ; interrupt for "write"
@@ -104,60 +58,9 @@ section     .text
         syscall
 
 section    .data
+    fmt:
+         db          "%i", 0xA, 0
+
     constmsg:
         db          "Here it is:", 0xA
-    fmt:
-        db          "%i", 0xA, 0
-    tab:
-        dq          C1, C2, C3
 
-
-;; -----------------------------------------------------------------------------
-;; MACOS
-;;
-;; Assemble, link and run using the following:
-;;   nasm case.asm -f macho64 -o prog.o && gcc -o prog prog.o && ./prog
-;; On Apple Silicon, compile using the following (will be executed using Rosetta):
-;;   nasm case.asm -f macho64 -o prog.o && clang -o prog -mmacosx-version-min=10.15 -arch x86_64 prog.o && ./prog
-;; -----------------------------------------------------------------------------
-; global      _main
-; extern      _atoi
-; extern      _printf
-; default     rel
-; section     .text
-;     _main:
-;         cmp         rdi, 2
-;         jl          exit
-;     initialize:
-;         push        rbx
-;         mov         rdi, [rsi + 8]
-;         call        _atoi
-;         cmp         rax, 3
-;         ja          dflt
-;         lea         rbx, [tab]
-;         jmp         [rbx + (rax - 1) * 8]
-;     C1:
-;         mov         rsi, 2
-;         jmp         print
-;     C2:
-;         mov         rsi, 3
-;         jmp         print
-;     C3:
-;         mov         rsi, 5
-;         jmp         print
-;     dflt:
-;         mov         rsi, 8
-;     print:
-;         lea         rdi, [fmt]
-;         xor         rax, rax
-;         call        _printf
-;     exit:
-;         pop         rbx
-;         mov         rax, 0x02000001
-;         xor         rdi, rdi
-;         syscall
-; section    .data
-;     fmt:
-;         db          "%i", 0xA, 0
-;     tab:
-;         dq          C1, C2, C3
