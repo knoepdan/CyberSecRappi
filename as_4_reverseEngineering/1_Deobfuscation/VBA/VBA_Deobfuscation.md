58384ae4f7d166f56c742825b48d41ae74f4b672
knoepdan <daniel.knoepfel@live.de>
2023-03-17 14:30:52 +0100

    vba deobfuscation

--- as_4_reverseEngineering/1_Deobfuscation/VBA/VBA_Deobfuscation.md
+++ as_4_reverseEngineering/1_Deobfuscation/VBA/VBA_Deobfuscation.md
@@ -14,19 +14,26 @@
      - Answer: 
        - Name mangling (meaningless names)
        - Control flow Rerouting -> lots of labels and Goto statements to make it harder to follow execution flow
-       - Encode parts to make it unreadable (Base64 encoding)
-
+       - Lots of dead code
+       - Identity operations (mainly with not initialized variables) 
+       - Data transformations (string concatenations)
+       - There seems to be some base64 encoding (detected by oleva) which seems to be hex. However, to me it seems that this part of the code was not used at all.
+            - with Vibermonkey, it is clear that a Powershell script is started. The script itself is base64 encoded (and further obfuscated) but I could not get/see from the output of olevba 
 3. See how far you get with manual deobfuscation of the code.
    1. Describe the steps you took.
         - Starting from the initially executed function, i started to rename labels, functions, variables by type (e.g. labelAA, inputString) etc. 
         - Merged some string initialization
         - Removed (or outcommented) unnecessary code, like unnecessary label jumps
-          - especially in the first method
+          - most labels are unnecessary and the entire code can be shrinked
+          - (most of the renames I did were obsolete after this point)
+        - end result see "extractedCode.vb"  (contains some dummy code to ensure actual code is never executed)
    2. Any idea what the VBA part of this malware does?
-
-
-
-
+        - it creates a winmgmts:win32_process object which in turn creates another object
+          - It passes some treated form of Tvh1u8793dltn9.Content but was not able determine what the value of this is
+          - It seems to create/start and object (process)
+            - with ViberMonkey it becomes clear that a powershell script is started (the Parameter is further obfuscated (basce64 etc.... it looks similar to the one in the next exercise))
+  
+  
 **Running olevba to extract and analze vb code in doc file**
 
 Create environment
@@ -36,6 +43,18 @@ Create environment
 - copy "Sample.doc" into ~/reversing/oletools"  (doc file from https://github.com/ti-ng/re-deobfuscation not checked as a) dangerous b) virus scanner might cause trouble)
 - run `pipenv run olevba ./Sample.doc` to analzye and get malicious doc file -> copy code to a file for analysis
 
-## VBA notes
 
+**Running ViperMonkey**
+- Go to folder ~/reversing and clone repo
+  - `git clone https://github.com/chezwicker/ViperMonkey.git`
+- Go to repo ("cd ViperMonkey")
+- Run vipermonkey: `./docker/dockermonkey.sh ~/reversing/Sample.doc`  (copy sample.doc there)
+    - had to adapt the the vipermonkey file dockermonkey.sh on line 49 (before empty line): "docker_id=$(docker run --network host --rm -d -t haroldogden/vipermonkey:latest)"   (apparently this is not necessary on a new live cd)    
+    - output file will provide detailed info (and execute parts)
+
+## Notes/varia
+VB notes
 - " _ " means statement continues on the next line. Example: `skuwd = Ga63a6ozyok1lu + Tvh1u8793dltn9 _`  
+
+Links:
+- https://perception-point.io/blog/malicious-office-macros-detecting-similarity-in-the-wild-2/ 
--- as_4_reverseEngineering/1_Deobfuscation/VBA/extractedCode.vb
+++ as_4_reverseEngineering/1_Deobfuscation/VBA/extractedCode.vb
@@ -9,114 +9,31 @@ End Sub
 
 
 Function startFuncWithoutLabels()
-inputStringAA = "wx [ sh binx [ sh bmx [ sh bgmx [ sh btx [ sh bx [ sh b" + "x [ sh bx [ sh bsx [ sh bx [ sh b" _ 
- + "x [ sh b:wx [ sh bx [ sh binx [ sh b3x [ sh b2x [ sh b_x [ sh b" + "x [ sh bpx [ sh b" _ 
- +  "x [ sh brox [ sh bx [ sh bcex [ sh bsx [ sh bsx [ sh bx [ sh b"
+    inputStringAA = "DUMMYCODETOENSUREITWILLNOTWORK" + "wx [ sh binx [ sh bmx [ sh bgmx [ sh btx [ sh bx [ sh b" + "x [ sh bx [ sh bsx [ sh bx [ sh b" _ 
+    + "x [ sh b:wx [ sh bx [ sh binx [ sh b3x [ sh b2x [ sh b_x [ sh b" + "x [ sh bpx [ sh b" _ 
+    +  "x [ sh brox [ sh bx [ sh bcex [ sh bsx [ sh bsx [ sh bx [ sh b"
 
-creatObInput = funcAAA(inputStringAA)
+    creatObInput = funcAAA(inputStringAA) ' winmgmts:win32_process
+    Set win32Process = CreateObject(creatObInput)
 
-Set V5rp8m_1bqwi1poyk = CreateObject(creatObInput)
+    'skuwd = Ga63a6ozyok1lu + Tvh1u8793dltn9.Content + P74x_w06z8wy
+    skuwd = Tvh1u8793dltn9.Content  ' other variables are empty
 
-njcnja = Mid(skuwd, (1 + 1 + 1 + 1), Len(skuwd))
-nnjasd = funcAAA(njcnja)
+    njcnja = Mid(skuwd, (1 + 1 + 1 + 1), Len(skuwd)) ' cuts first few characters
+    nnjasd = funcAAA(njcnja)  ' does a simple replace ...
 
-V5rp8m_1bqwi1poyk.Create nnjasd, Koy_r2oxzs1, X2yj58n39t50co
+    win32Process.Create nnjasd, Koy_r2oxzs1, X2yj58n39t50co
 
 End Function
 
 Function funcAAA(paramStringA)
-On Error Resume Next
-   GoTo labelOnError
-Set wrpigDnBA = bHGFAGJ
-    Dim fUGOALvdN, FKISJTLG, OpNHJEa As Long
-    Dim ZlnBbxF As Word.Paragraph
-    Dim pxjzGA() As Byte
-    For Each ZlnBbxF In Tvh1u8793dltn9.Paragraphs
-        pxjzGA = ZlnBbxF.Range
-        dscc = "sadsaccc" & ZlnBbxF.Range
-        FKISJTLG = UBound(pxjzGA) - 1
-        fUGOALvdN = 0
-Set xNIlBBInl = OaOIEKmCA
-        Do Until FKISJTLG > FKISJTLG
-            If pxjzGA(FKISJTLG) = 46 Or FKISJTLG = FKISJTLG Then
-                dscc = "sasdsacc" & (fUGOALvdN / 2) + 1 & " to " & (FKISJTLG / 2) + 1 & MidB$(pxjzGA, fUGOALvdN + 1, FKISJTLG - fUGOALvdN + 3)
-                fUGOALvdN = FKISJTLG + 2
-            End If
-            FKISJTLG = FKISJTLG + 2
-        Loop
-    Next
-labelOnError:
-E4u6ubi3v5l2 = funcBBB(paramStringA)
-funcAAA = = E4u6ubi3v5l2 'K532dwnyk0pybrc = E4u6ubi3v5l2
-   GoTo labelEndFuncAAA
-
-Set phkpFqFCH = DHwdFs
-    Dim nHiSH, jHDSG, udnviH As Long
-    Dim DLwSlnDF As Word.Paragraph
-    Dim pXPTCf() As Byte
-    For Each DLwSlnDF In Tvh1u8793dltn9.Paragraphs
-        pXPTCf = DLwSlnDF.Range
-        dscc = "sadsaccc" & DLwSlnDF.Range
-        jHDSG = UBound(pXPTCf) - 1
-        nHiSH = 0
-Set seTGCvRG = mwvhyA
-        Do Until jHDSG > jHDSG
-            If pXPTCf(jHDSG) = 46 Or jHDSG = jHDSG Then
-                dscc = "sasdsacc" & (nHiSH / 2) + 1 & " to " & (jHDSG / 2) + 1 & MidB$(pXPTCf, nHiSH + 1, jHDSG - nHiSH + 3)
-                nHiSH = jHDSG + 2
-            End If
-            jHDSG = jHDSG + 2
-        Loop
-    Next
-
-Set ZtgGUHFGJ = prgAO
-    Dim TMQhTRa, LZepVwu, JPHDBd As Long
-    Dim bkUZDN As Word.Paragraph
-    Dim QNtsSHe() As Byte
-    For Each bkUZDN In Tvh1u8793dltn9.Paragraphs
-        QNtsSHe = bkUZDN.Range
-        dscc = "sadsaccc" & bkUZDN.Range
-        LZepVwu = UBound(QNtsSHe) - 1
-        TMQhTRa = 0
-Set MxAtNhGI = imnrzOF
-        Do Until LZepVwu > LZepVwu
-            If QNtsSHe(LZepVwu) = 46 Or LZepVwu = LZepVwu Then
-                dscc = "sasdsacc" & (TMQhTRa / 2) + 1 & " to " & (LZepVwu / 2) + 1 & MidB$(QNtsSHe, TMQhTRa + 1, LZepVwu - TMQhTRa + 3)
-                TMQhTRa = LZepVwu + 2
-            End If
-            LZepVwu = LZepVwu + 2
-        Loop
-    Next
-' XxLEEC:
-' funcAAA = = E4u6ubi3v5l2 'K532dwnyk0pybrc = E4u6ubi3v5l2
-'    GoTo labelEndFuncAAA
-Set OaVnI = UmQHurWB
-    Dim zxEzinCG, EHISACDA, aBRvB As Long
-    Dim XdfYSIXX As Word.Paragraph
-    Dim wWvlxHJH() As Byte
-    For Each XdfYSIXX In Tvh1u8793dltn9.Paragraphs
-        wWvlxHJH = XdfYSIXX.Range
-        dscc = "sadsaccc" & XdfYSIXX.Range
-        EHISACDA = UBound(wWvlxHJH) - 1
-        zxEzinCG = 0
-Set wVEbaDF = WhmkB
-        Do Until EHISACDA > EHISACDA
-            If wWvlxHJH(EHISACDA) = 46 Or EHISACDA = EHISACDA Then
-                dscc = "sasdsacc" & (zxEzinCG / 2) + 1 & " to " & (EHISACDA / 2) + 1 & MidB$(wWvlxHJH, zxEzinCG + 1, EHISACDA - zxEzinCG + 3)
-                zxEzinCG = EHISACDA + 2
-            End If
-            EHISACDA = EHISACDA + 2
-        Loop
-    Next
-labelEndFuncAAA:
+    ' basically returns value of function b
+    funcAAA = funcBBB(paramStringA)
 End Function
 
 
-
-
-
 Function funcBBB(inputString)
-    funcBBB = Replace(inputString, "x [ sh b", Zi0fdg4qf12t) ' winmgmts:win32_process
+    funcBBB = Replace(inputString, "x [ sh b", Zi0fdg4qf12t) 'returns winmgmts:win32_process when fed with main string
 End Function
 
 
@@ -132,6 +49,13 @@ End Function
 ' |          |                    |used to obfuscate strings (option --decode to|
 ' |          |                    |see all)                                     |
 ' +----------+--------------------+---------------------------------------------+
+'|Base64    |'li\x1c'         |sadsaccc                                     |
+'|String    |                    |                                             |
+'|Base64    |'\x1d\x1c'      |sasdsacc                                     |
+'|String    |                    |                                             |
+'+----------+--------------------+---------------------------------------------+
+
+
 
 
 
