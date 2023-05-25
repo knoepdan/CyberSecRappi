25a465d0c5d00eb18488627ad8a0932226d0f2d7
knoepdan <daniel.knoepfel@live.de>
2023-05-25 23:10:12 +0200

    Example C# in Linux

---
+++ as_3_programming/_Interop/Linux/CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        // to build: "dotnet build CSharp.csproj"
+        // to run (after building): "dotnet run CSharp.dll"   (here dll is in same directory)
+        // to debug: ?? didnt work out of the box with Linux and VS Code
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
